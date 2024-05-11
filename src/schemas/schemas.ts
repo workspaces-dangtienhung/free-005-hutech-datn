ef409084d35f35299ee735c30cdeb2b72149cfc3
Dat Nguyen <datnguyen01122003@gmail.com>
2024-05-11 23:40:28 +0700

    fix message validate

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -13,13 +13,13 @@ export const signUpSchema = Yup.object({
   userName: Yup.string().required("Tên không được để trống"),
   phone: Yup.string()
     .matches(/^0\d{9}$/, {
-      message: "Số điện thoại phải bắt đầu bằng số 0 và có 10 chữ số",
+      message: "Số điện thoại không đúng định dạng",
       excludeEmptyString: true,
     })
     .required("Số điện thoại không được để trống"),
   email: Yup.string()
-    .email("Email không hợp lệ")
-    .matches(/@gmail.com$/, "Email phải có đuôi gmail.com")
+    .email("Email không đúng định dạng")
+    .matches(/@gmail.com$/, "Email không đúng định dạng")
     .required("Email không được để trống"),
   password: Yup.string()
     .min(6, "Mật khẩu phải từ 6 ký tự!")
